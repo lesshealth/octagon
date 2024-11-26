3868d7f9dc6c72b98a6b03f9dddfa611e542a9f2
Andrey Popovich <lessshealth@gmail.com>
2024-11-27 04:11:41 +1000

    added /static and /dynamic

--- index.js
+++ index.js
@@ -6,4 +6,36 @@ app.get("/", function(request, response){
      
     response.send("<h2>Привет, Октагон!</h2>");
 });
+
+
+app.get("/", function (request, response) {
+    response.send("<h2>Привет, Октагон!</h2>");
+});
+
+
+app.get("/static", function (request, response) {
+    response.json({
+        header: "Hello",
+        body: "Octagon NodeJS Test"
+    });
+});
+
+
+app.get("/dynamic", function (request, response) {
+    const { a, b, c } = request.query;
+
+    if (!a || !b || !c || isNaN(a) || isNaN(b) || isNaN(c)) {
+        return response.json({
+            header: "Error"
+        });
+    }
+
+
+    const result = (a * b * c) / 3;
+
+    response.json({
+        header: "Calculated",
+        body: result.toString()
+    });
+});
 app.listen(3000);
